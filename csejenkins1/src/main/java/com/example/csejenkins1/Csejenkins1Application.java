5ba543af28c22dba1ce04ef669df2d3221e4e403
Nakka Sravanti <nakkasravanti8@gmail.com>
2025-09-22 12:29:25 +0530

    this is my cse jenkins

---
+++ csejenkins1/src/main/java/com/example/csejenkins1/Csejenkins1Application.java
@@ -0,0 +1,13 @@
+package com.example.csejenkins1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csejenkins1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Csejenkins1Application.class, args);
+	}
+
+}
---
+++ csejenkins1/src/test/java/com/example/csejenkins1/Csejenkins1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.csejenkins1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Csejenkins1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
